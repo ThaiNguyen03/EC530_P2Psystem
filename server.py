363ec8c9c4e758a5e6b65897037f44066adc80a0
Thainguyenluong <Thainguyenluong@github.com>
2024-04-08 18:04:38 -0400

    add synchronization

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
-
 import socket
 import threading
+from queue import Queue
 
 class Server:
-    def __init__(self, host = '127.0.0.1', port = 55555):
+    def __init__(self, host='127.0.0.1', port=55555):
         self.host = host
         self.port = port
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,16 +11,38 @@ class Server:
         self.server.listen()
         self.clients = []
         self.nicknames = []
+        self.message_buffer = Queue()  # Message buffer for synchronization
 
-    def broadcast(self, message):
-        for client in self.clients:
-            client.send(message)
+    def broadcast(self, message, exclude=None):
+        if exclude is not None:
+            for client in self.clients:
+                if client != exclude:
+                    try:
+                        client.send(message)
+                    except:
+                        pass  # Handle error or removal of client
+        else:
+            for client in self.clients:
+                try:
+                    client.send(message)
+                except:
+                    pass  # Handle error or removal of client
+
+        # Empty the buffer and send to the newly connected client if exclude is specified
+        if exclude:
+            while not self.message_buffer.empty():
+                message = self.message_buffer.get()
+                try:
+                    exclude.send(message)
+                except:
+                    pass  # Handle error
 
     def handle(self, client):
         while True:
             try:
                 message = client.recv(1024)
                 self.broadcast(message)
+                self.message_buffer.put(message)  # Add incoming messages to buffer
             except:
                 index = self.clients.index(client)
                 self.clients.remove(client)
@@ -41,7 +63,7 @@ class Server:
             self.clients.append(client)
 
             print(f'Nickname of the client is {nickname}!')
-            self.broadcast(f'{nickname} joined the chat!'.encode('ascii'))
+            self.broadcast(f'{nickname} joined the chat!'.encode('ascii'), exclude=client)
             client.send('Connected to the server!'.encode('ascii'))
 
             thread = threading.Thread(target=self.handle, args=(client,))
